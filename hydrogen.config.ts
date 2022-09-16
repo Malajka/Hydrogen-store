c64269b151a517e34c4d7f3e38559162bdc4a558
Bieniecka <Monika.Bieniecka@reckitt.com>
2022-09-16 15:55:33 +0200

    initial template

---
+++ hydrogen.config.ts
@@ -0,0 +1,9 @@
+import {defineConfig} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    storeDomain: 'finish-test.myshopify.com',
+    storefrontToken: '882ed584882a6d19e42f7d5a54cdf819',
+    storefrontApiVersion: '2022-07',
+  },
+});
